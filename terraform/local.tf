d2d4e32577a99431a791a6ab3864fa6f6a4922f8
Alokkodzz <alok63579@gmail.com>
2025-05-11 14:09:59 +0000

    Update local.tf
    new ami

--- terraform/local.tf
+++ terraform/local.tf
@@ -1,5 +1,5 @@
 locals {
-  ami            = "ami-0977e21ac9ee2aa2f"
+  ami            = "ami-02fbcd572993ac1a9"
   instance_type  = "t2.micro"
   subnet_id      = "subnet-0802eb25e45a54f45"
   VPC_availability_zone = ["us-east-1a", "us-east-1b", "us-east-1c"]
